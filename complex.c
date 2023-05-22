3224c05cfbca5adaeb93d13052d5ab208b4ac265
dako26 <111310920+dako26@users.noreply.github.com>
2023-05-22 21:32:42 +0300

    a`

--- complex.c
+++ complex.c
@@ -19,3 +19,24 @@ complex sub_comp(complex *number1,complex *number2){
     result.img=number1->img - number2->img;
     return result;
 }
+complex mult_comp_real(complex *number1,double number2){
+    complex result;
+    result.img=number1->img*number2;
+    result.real=number1->real*number2;
+    return result;
+}
+complex mult_comp_img(complex *number1,double number2){
+    complex result;
+    result.img=number1->real*number2;
+    result.real=number1->img*number2*-1;
+    return result;
+}
+complex mult_comp_comp(complex *number1,complex *number2){
+    complex result;
+    result.real=((number1->real*number2->real)-(number1->img*number2->img));
+    result.img=((number1->real*number2->img)+(number1->img*number2->real));
+    return result;
+}
+double abs_comp(complex *number1){
+    return sqrt(pow(number1->real,2)+ pow(number1->img,2));
+}
--- complex.h
+++ complex.h
@@ -10,3 +10,7 @@ void read_comp(complex *number,double real,double img);
 void print_comp(complex *number);
 complex add_comp(complex *number1,complex *number2);
 complex sub_comp(complex *number1,complex *number2);
+complex mult_comp_real(complex *number1,double number2);
+complex mult_comp_img(complex *number1,double number2);
+complex mult_comp_comp(complex *number1,complex *number2);
+double abs_comp(complex *number1);
